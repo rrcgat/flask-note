fb86833992ee21a5b0faf1d05ef4715b0420c032
rrcgat <rrcgat@gmail.com>
2018-06-02 09:20:44 +0800

    Add database module

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql://xbb:TnI5SU5L@127.0.0.1:5432/note')
+db_session = scoped_session(sessionmaker(autocommit=False
+                                         autoflush=False
+                                         bind=engine))
+
+Base = declarative_base()
+
+def init_db():
+    import models
+    Base.metadata.create_all(bind=engine)
