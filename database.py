5035af0c1389e4cfbd42ab33eec567d2429a2f84
rrcgat <rrcgat@gmail.com>
2018-06-02 16:22:04 +0800

    Fix syntax error

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('postgresql://xbb:TnI5SU5L@127.0.0.1:5432/note')
-db_session = scoped_session(sessionmaker(autocommit=False
-                                         autoflush=False
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
                                          bind=engine))
 
 Base = declarative_base()
