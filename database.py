7477b4cb10948d6ee2f3ac6ae2302054813fda9f
rrcgat <rrcgat@gmail.com>
2018-06-13 13:54:14 +0800

    为数据库表对应的对象添加查询功能

--- database.py
+++ database.py
@@ -8,6 +8,7 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          bind=engine))
 
 Base = declarative_base()
+Base.query = db_session.query_property()
 
 def init_db():
     from app import models
