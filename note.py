48571527507b2454c140eb6a61c783e9cfd527f6
rrcgat <rrcgat@gmail.com>
2018-06-22 15:13:21 +0800

    添加删除功能

--- note.py
+++ note.py
@@ -13,6 +13,18 @@ def index():
     notebooks = NoteInfo.query.all()[:10]
     return render_template('index.html', notebooks=notebooks)
 
+@app.route('/d', methods=['POST'])
+def delete():
+    ts = request.values.get('ts')
+    _type = request.values.get('type')
+    if _type =='book':
+        data = Notebooks.query.filter_by(ts=ts).first()
+    else:
+        data = NoteInfo.query.filter_by(ts=ts).first()
+    db_session.delete(data)
+    db_session.commit()
+    return '1'
+
 @app.route('/u', methods=['POST'])
 def upload():
     if request.method == 'POST':
